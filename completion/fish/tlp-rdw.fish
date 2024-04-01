16862636e6b4bd81e975b5b7564494c8982ff0f6
Thomas Koch <linrunner@gmx.net>
2024-04-01 17:36:44 +0200

    bluetooth, nfc, wifi, wwan, tlp-rdw --version
    Reference:
    * https://github.com/linrunner/TLP/issues/462

--- completion/fish/tlp-rdw.fish
+++ completion/fish/tlp-rdw.fish
@@ -5,3 +5,4 @@ set -l tlp_rdw_commands enable disable
 complete -c tlp-rdw -f
 complete -c tlp-rdw -n "not __fish_seen_subcommand_from $tlp_rdw_commands" -a enable -d 'Enable RDW actions'
 complete -c tlp-rdw -n "not __fish_seen_subcommand_from $tlp_rdw_commands" -a disable -d 'Disable RDW actions'
+complete -c tlp-rdw -n "not __fish_seen_subcommand_from $tlp_rdw_commands" -l version -d 'Print TLP version'
--- completion/fish/tlp.fish
+++ completion/fish/tlp.fish
@@ -16,19 +16,19 @@ if test $current_command = "tlp"
     end
 
     complete -c tlp -f
-    complete -c tlp -f -n "not __fish_seen_subcommand_from $tlp_commands" -a start -d "Start tlp and apply power saving profile for the actual power source"
-    complete -c tlp -f -n "not __fish_seen_subcommand_from $tlp_commands" -a bat -d "Apply battery profile and enter manual mode"
-    complete -c tlp -f -n "not __fish_seen_subcommand_from $tlp_commands" -a ac -d "Apply AC profile and enter manual mode"
-    complete -c tlp -f -n "not __fish_seen_subcommand_from $tlp_commands" -a usb -d "Enable autosuspend for all USB devices except excluded"
-    complete -c tlp -f -n "not __fish_seen_subcommand_from $tlp_commands" -a bayoff -d "Turn off optical drive in UltraBay/MediaBay"
-    complete -c tlp -f -n "not __fish_seen_subcommand_from $tlp_commands" -a setcharge -d "Change charge thresholds temporarily"
-    complete -c tlp -f -n "not __fish_seen_subcommand_from $tlp_commands" -a fullcharge -d "Charge battery to full capacity"
-    complete -c tlp -f -n "not __fish_seen_subcommand_from $tlp_commands" -a chargeonce -d "Charge battery to the stop charge threshold once (ThinkPads only)"
-    complete -c tlp -f -n "not __fish_seen_subcommand_from $tlp_commands" -a discharge -d "Force a complete discharge of the battery (ThinkPads only)"
-    complete -c tlp -f -n "not __fish_seen_subcommand_from $tlp_commands" -a recalibrate -d "Perform a battery recalibration (ThinkPads only)"
-    complete -c tlp -f -n "not __fish_seen_subcommand_from $tlp_commands" -a diskid -d "Print disk ids for configured drives"
-    complete -c tlp -l version -d 'Print TLP version'
-    complete -c tlp -f -n "__fish_seen_subcommand_from $tlp_commands[6..10] && not __fish_seen_subcommand_from $bats" -a "$bats"
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a start -d "Start tlp and apply power saving profile for the actual power source"
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a bat -d "Apply battery profile and enter manual mode"
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a ac -d "Apply AC profile and enter manual mode"
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a usb -d "Enable autosuspend for all USB devices except excluded"
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a bayoff -d "Turn off optical drive in UltraBay/MediaBay"
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a setcharge -d "Change charge thresholds temporarily"
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a fullcharge -d "Charge battery to full capacity"
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a chargeonce -d "Charge battery to the stop charge threshold once (ThinkPads only)"
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a discharge -d "Force a complete discharge of the battery (ThinkPads only)"
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a recalibrate -d "Perform a battery recalibration (ThinkPads only)"
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a diskid -d "Print disk ids for configured drives"
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -l version -d 'Print TLP version'
+    complete -c tlp -n "__fish_seen_subcommand_from $tlp_commands[6..10] && not __fish_seen_subcommand_from $bats" -a "$bats"
 end
 
 if contains $current_command $tlp_rf_devices
@@ -36,4 +36,5 @@ if contains $current_command $tlp_rf_devices
     complete -c $current_command -n "not __fish_seen_subcommand_from $tlp_rf_devices_commands" -a on -d 'Switch device on'
     complete -c $current_command -n "not __fish_seen_subcommand_from $tlp_rf_devices_commands" -a off -d 'Switch device off'
     complete -c $current_command -n "not __fish_seen_subcommand_from $tlp_rf_devices_commands" -a toggle -d 'Toggle device state'
+    complete -c $current_command -n "not __fish_seen_subcommand_from $tlp_rf_devices_commands" -l version -d 'Print TLP version'
 end
