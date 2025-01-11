97d54d985a69f5e822c54709fd6e3b3f1202a3fb
Thomas Koch <linrunner@gmx.net>
2025-01-11 20:49:29 +0100

    test_all.sh: run all tests in one batch

---
+++ unit-tests/test_all.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+readonly TESTLIB="./test-func"
+# shellcheck disable=SC1090
+. $TESTLIB || {
+    printf "Error: missing library %s\n" "${TESTLIB}" 1>&2
+    exit 70
+}
+
+start_report
+
+./test-cpufreq.sh
+./test-gpufreq.sh
+./test-bc_all.sh
+
+print_report
