dac3bf81382985bd87e795843f21ff5cf477fe4c
Thomas Koch <linrunner@gmx.net>
2025-08-01 13:44:22 +0200

    zsh/fish completions: add power profile subcommands

--- completion/fish/tlp.fish
+++ completion/fish/tlp.fish
@@ -1,6 +1,6 @@
 # Fish shell completion for tlp, radio device command: bluetooth nfc wifi wwan, and run-on command: run-on-ac run-on-bat
 
-set -l tlp_commands start bat ac usb bayoff setcharge fullcharge discharge recalibrate chargeonce diskid
+set -l tlp_commands start performance balanced power-saver ac bat usb bayoff setcharge fullcharge discharge recalibrate chargeonce diskid
 set -l tlp_rf_devices bluetooth nfc wifi wwan
 set -l tlp_rf_devices_commands on off toggle
 set -l runon_commands run-on-ac run-on-bat
@@ -17,16 +17,19 @@ if test $current_command = "tlp"
     end
 
     complete -c tlp -f
-    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a start -d "Start tlp and apply power saving profile for the actual power source"
-    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a bat -d "Apply battery profile and enter manual mode"
-    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a ac -d "Apply AC profile and enter manual mode"
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a start -d "Start tlp and apply power profile for the actual power source"
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a performance -d 'Apply "performance" profile'
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a balanced -d 'Apply "balanced" profile'
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a power-saver -d 'Apply "power-saver" profile'
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a ac -d 'Apply "performance" profile and enter manual mode'
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a bat -d 'Apply "balanced" profile and enter manual mode'
     complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a usb -d "Enable autosuspend for all USB devices except excluded"
     complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a bayoff -d "Turn off optical drive in UltraBay/MediaBay"
     complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a setcharge -d "Change charge thresholds temporarily"
     complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a fullcharge -d "Charge battery to full capacity"
-    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a chargeonce -d "Charge battery to the stop charge threshold once (ThinkPads only)"
-    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a discharge -d "Force a complete discharge of the battery (ThinkPads only)"
-    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a recalibrate -d "Perform a battery recalibration (ThinkPads only)"
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a chargeonce -d "Charge battery to the stop charge threshold once"
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a discharge -d "Force a complete discharge of the battery"
+    complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a recalibrate -d "Perform a battery recalibration"
     complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -a diskid -d "Print disk ids for configured drives"
     complete -c tlp -n "not __fish_seen_subcommand_from $tlp_commands" -l version -d 'Print TLP version'
     complete -c tlp -n "__fish_seen_subcommand_from $tlp_commands[6..10] && not __fish_seen_subcommand_from $bats" -a "$bats"
